5555d7547b74d9d314010774befc6d045e749192
CallumHoughton18 <Callum.Houghton13@hotmail.co.uk>
2020-03-02 20:13:30 +0000

    WIP: Added TrainingDiagnostics class to simplify matplotlib functionality.

--- src/mushroom-classifier/TrainingDiagnostics.py
+++ src/mushroom-classifier/TrainingDiagnostics.py
@@ -1,38 +1,52 @@
 import matplotlib.pyplot as plt
 import matplotlib.axes as ax
 import numpy as np
+import os
 
-def plot_cost(cost_values):
-    plt.title("Cost over time")
-    plt.ylabel("Cost")
-    plt.xlabel("iteration step")
-    plt.plot(cost_values)
-    plt.savefig("cost.png")
+class TrainingDiagnostics():
+    def __init__(self):
+        plt.cla()
+        self.mainfig = plt.figure()
 
-def plot_learning_curve(X, y, X_validation, y_validation, model, training_examples_steps=100):
-    m = X.shape[0]
-    error_training = []
-    error_validation = []
-    steps = range(training_examples_steps, 500, training_examples_steps)
-    for i in steps:
-        X_subset = X[:i,:]
-        y_subset = y[:i]
-        model.train(X_subset, y_subset)
+    def plot_cost(self, cost_values):
+        ax = self.mainfig.add_subplot(221)
+        ax.title.set_text("Cost over time")
+        ax.set_ylabel("Cost")
+        ax.set_xlabel("iteration step")
+        ax.plot(cost_values)
 
-        cost_hypothesis = model.hypothesis(X_subset, model.theta)
-        cost_training_set = model.cost_function(cost_hypothesis, y_subset)
-        error_training.append(cost_training_set)
+    def plot_learning_curve(self, X, y, X_validation, y_validation, model, max_training_size=None, training_examples_steps=100):
+        max_training_size = X.shape[0] if max_training_size==None else max_training_size
+        error_training = []
+        error_validation = []
+        steps = range(training_examples_steps, max_training_size, training_examples_steps)
+        for i in steps:
+            X_subset = X[:i,:]
+            y_subset = y[:i]
+            model.train(X_subset, y_subset)
 
-        cost_validation_hypothesis = model.hypothesis(X_validation, model.theta)
-        cost_validation_set = model.cost_function(cost_validation_hypothesis, y_validation)
-        error_validation.append(cost_validation_set)
+            cost_hypothesis = model.hypothesis(X_subset, model.theta)
+            cost_training_set = model.cost_function(cost_hypothesis, y_subset)
+            error_training.append(cost_training_set)
 
-    plt.title("Learning Curve")
-    plt.ylabel("Cost")
-    plt.xlabel("# Of Training Examples")
-    plt.plot(steps, error_training)
-    fig = plt.plot(steps, error_validation)    
-    return fig
+            cost_validation_hypothesis = model.hypothesis(X_validation, model.theta)
+            cost_validation_set = model.cost_function(cost_validation_hypothesis, y_validation)
+            error_validation.append(cost_validation_set)
+
+        ax = self.mainfig.add_subplot(222)
+        ax.title.set_text("Learning Curves")
+        ax.set_ylabel("Cost")
+        ax.set_xlabel("# Of Training Examples")
+        ax.plot(steps, error_training)
+        ax.plot(steps, error_validation)
+
+    def save_diagostics(self, folder_path):
+        if not os.path.exists(folder_path):
+            os.mkdir(folder_path)
+            
+        figure_path = os.path.join(folder_path, 'diagnostics.png')
+        self.mainfig.savefig(figure_path)
+
 
 
 
--- src/mushroom-classifier/test.py
+++ src/mushroom-classifier/test.py
@@ -2,32 +2,33 @@ import numpy as np
 import pickle
 from LogisticRegression import LogisticRegression
 from DataCleaner import DataCleaner
-from TrainingDiagnostics import plot_cost, plot_learning_curve
+from TrainingDiagnostics import TrainingDiagnostics
 
 def log(message):
     print(message)
-
+    
+training_diagnostics = TrainingDiagnostics()
 data_cleaner = DataCleaner('../files/mushrooms.csv','class','p')
 [train_X, test_X, train_y, test_y]  = data_cleaner.clean()
 
 model_for_learning_curve = LogisticRegression(learning_rate=1, num_iter=500, verbose=False, fit_intercept=False)
-learning_curve = plot_learning_curve(train_X, train_y, test_X, test_y, model_for_learning_curve)
-learning_curve.savefig("learningcurve.jpg")
+training_diagnostics.plot_learning_curve(train_X, train_y, test_X, test_y, model_for_learning_curve, max_training_size=3000)
 
-# model = LogisticRegression(learning_rate=1, num_iter=500, verbose=True)
-# model.setup_logger(log)
-# [thetas, costs] = model.train(train_X, train_y)
-# print('plotting costs...')
-# plot_cost(costs)
+model = LogisticRegression(learning_rate=1, num_iter=500, verbose=True)
+model.setup_logger(log)
+[thetas, costs] = model.train(train_X, train_y)
+training_diagnostics.plot_cost(costs)
 
-# preds = model.predict(test_X)
-# accuracy = (preds == test_y).mean()
-# print(accuracy)
+training_diagnostics.save_diagostics('./')
 
-# filename = "test_model.sav"
-# pickle.dump(model, open(filename, "wb"))
+preds = model.predict(test_X)
+accuracy = (preds == test_y).mean()
+print(accuracy)
 
-# loaded_model = pickle.load(open(filename, "rb"))
-# loaded_accuracy = (loaded_model.predict(test_X) == test_y).mean()
-# print(loaded_accuracy)
+filename = "test_model.sav"
+pickle.dump(model, open(filename, "wb"))
+
+loaded_model = pickle.load(open(filename, "rb"))
+loaded_accuracy = (loaded_model.predict(test_X) == test_y).mean()
+print(loaded_accuracy)
 
