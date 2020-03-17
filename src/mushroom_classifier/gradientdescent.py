Added further gradientdescent tests, fixed issue with regularization calculation

src/mushroom_classifier/gradientdescent.py
@@ -25,8 +25,15 @@ class GradientDescent():
         return [self.theta, costs]
 
     def __calc_regulization(self, lambdaTerm):
-        theta_for_reg = self.theta[:1:] if self.intercept_added else self.theta
+        theta_for_reg = None
+        if self.intercept_added:
+            theta_for_reg = np.copy(self.theta)
+            theta_for_reg[0] = 0
+        else:
+            theta_for_reg = self.theta
+        #theta_for_reg = self.theta[1:] if self.intercept_added else self.theta
         regularlizedTerm = np.dot((lambdaTerm/self.y.shape[0]), theta_for_reg)
+        #print(regularlizedTerm)
         return regularlizedTerm
 
 

src/test_mushroom_classifier/base_test.py
@@ -5,4 +5,11 @@ class TestBase(unittest.TestCase):
     def setUp(self):
         self.test_X = np.ndarray(shape=(2,2), dtype=int, buffer=np.array([[1,2],[3,4]]))
         self.test_y = np.ndarray(shape=(2,), dtype=int, buffer=np.array([1, 0]))
-        self.test_theta = [0.2, 0.8]
+        self.test_theta = np.zeros(self.test_X.shape[1])
+
+    def get_x_and_theta_with_intercept(self):
+        intercept = np.ones((self.test_X.shape[0], 1))
+        x_with_intercept = np.concatenate((intercept, self.test_X), axis=1)
+        theta_with_intercept = np.zeros(x_with_intercept.shape[1])
+        
+        return [x_with_intercept, theta_with_intercept]

src/test_mushroom_classifier/test_gradientdescent.py
@@ -10,10 +10,21 @@ class GradientDescentTests(TestBase):
     def mock_hypothesis(self, X, theta):
         return 1
 
-    def test_hypothesis_happypath(self):
+    def test_calculate_happypath(self):
         expected_costs=[1, 1, 1, 1, 1]
-        expected_theta=np.ndarray(shape=(2,),dtype=float, buffer=np.array([-2.9 , -3.85]))
-        sut = GradientDescent(self.mock_hypothesis, self.mock_cost_function, self.test_X, self.test_y, self.test_theta, intercept_added=False)
+        expected_theta=np.ndarray(shape=(2,),dtype=float, buffer=np.array([-2.90625 , -3.875]))
+
+        self.calculate_assertion(self.test_X, self.test_theta ,expected_costs, expected_theta, add_intercept=False)
+
+    def test_calculate_intercepted_added(self):
+        expected_costs=[1, 1, 1, 1, 1]
+        [test_x_with_intercept , test_theta_with_intercept] = self.get_x_and_theta_with_intercept()
+        expected_theta=np.ndarray(shape=(3,),dtype=float, buffer=np.array([-2.5, -2.90625, -3.875]))
+
+        self.calculate_assertion(test_x_with_intercept, test_theta_with_intercept ,expected_costs, expected_theta, add_intercept=True)
+
+    def calculate_assertion(self, test_X, test_theta, expected_costs, expected_theta, add_intercept):
+        sut = GradientDescent(self.mock_hypothesis, self.mock_cost_function, test_X, self.test_y, test_theta, intercept_added=add_intercept)
 
         [actual_theta, actual_costs] = sut.calculate(5)
 

src/test_mushroom_classifier/test_logisticregression.py
@@ -5,12 +5,12 @@ from test_mushroom_classifier.base_test import TestBase
 
 class LogisticRegressionTests(TestBase):
     def test_hypothesis_happypath(self):
-        expected_hypothesis=[0.85814894, 0.97811873]
+        expected_hypothesis=[0.5, 0.5]
         sut = LogisticRegression()
 
         actual_hypothesis = sut.hypothesis(self.test_X, self.test_theta)
 
-        np.testing.assert_array_almost_equal(expected_hypothesis, actual_hypothesis, decimal=8)
+        np.testing.assert_array_almost_equal(expected_hypothesis, actual_hypothesis, decimal=1)
 
     def test_cost_function_happypath(self):
         expected_cost = 0.22314355