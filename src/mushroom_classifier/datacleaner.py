6fa6da3d82e93059503032765431783418a3206b
CallumHoughton18 <Callum.Houghton13@hotmail.co.uk>
2020-03-19 20:14:10 +0000

    Added function to datacleaner to encode unseen data against original dataset

--- src/mushroom_classifier/datacleaner.py
+++ src/mushroom_classifier/datacleaner.py
@@ -5,16 +5,21 @@ from sklearn.model_selection import train_test_split
 class DataCleaner:
     train = None
 
-    """Encodes the given dataset"""
+    """Encodes and splits the given dataset"""
     def __init__(self, csvData, classification_column_name, truth_value):
         self.train = pd.read_csv(csvData)
         self.classification_column_name = classification_column_name
         self.truth_value = truth_value
 
     def clean(self):
-        X = self.__onehot_encode(self.classification_column_name)
-        y = self.__extract_y_values(self.classification_column_name, self.truth_value)
-        return train_test_split(X, y, train_size=0.85, test_size=0.15)
+        self.X = self.__onehot_encode(self.classification_column_name)
+        self.y = self.__extract_y_values(self.classification_column_name, self.truth_value)
+        return train_test_split(self.X, self.y, train_size=0.85, test_size=0.15)
+
+    def clean_new_unseen_data(self, new_data):
+        new_X = pd.get_dummies(new_data)
+        new_X.reindex(columns = self.X.columns,fill_value=0)
+        return new_X
 
     def __onehot_encode(self, classification_column_name):
         X = pd.get_dummies(self.train.drop(classification_column_name, axis=1))
