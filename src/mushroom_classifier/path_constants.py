df6c29a04d140d2cd9535894e223283d806820e3
CallumHoughton18 <Callum.Houghton13@hotmail.co.uk>
2020-03-24 20:09:07 +0000

    Finalized training script, added paths as environ vars, added copying of successful model to active current_model directory.

---
+++ src/mushroom_classifier/path_constants.py
@@ -0,0 +1,5 @@
+from os import environ
+
+DATASET_DIR = environ.get("DATASET_DIR")
+CURRENT_MODEL_DIR = environ.get("CURRENT_MODEL_DIR")
+TRAINED_MODELS_DIR = environ.get("TRAINED_MODELS_DIR")
--- src/mushroom_classifier/train.py
+++ src/mushroom_classifier/train.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
-from os import path
+from os import path, environ, makedirs
 from pickle import load
+from shutil import rmtree, copytree
 
 from mushroom_classifier.logisticregression import LogisticRegression
 from mushroom_classifier.datacleaner import DataCleaner
@@ -12,8 +13,9 @@ from mushroom_classifier.path_constants import DATASET_DIR, CURRENT_MODEL_DIR, T
 def log(message):
     print(message)
 
+print(DATASET_DIR)
 models_folder = TRAINED_MODELS_DIR
-dataset_path = path.join(DATASET_DIR, "/mushrooms.csv")
+dataset_path = path.join(DATASET_DIR, "mushrooms.csv")
 training_diagnostics = TrainingDiagnostics()
 model_storage = ModelStorage(models_folder)
 dataset = pd.read_csv(dataset_path)
@@ -52,8 +54,19 @@ assert loaded_accuracy > 0.95
 data_cleaner = DataCleaner(dataset,'class','p')
 data_cleaner.columns_for_index = loaded_columns
 
-unseen_test_data_path = path.join(DATASET_DIR, "/unseen_mushrooms.csv")
+unseen_test_data_path = path.join(DATASET_DIR, "unseen_mushrooms.csv")
 unseen_test_data = pd.read_csv(unseen_test_data_path)
 unseen_test_data_cleaned = data_cleaner.clean_new_unseen_data(unseen_test_data)
 unseen_preds = loaded_model.predict(unseen_test_data_cleaned)
+
 np.testing.assert_array_equal(unseen_preds, [True, False],"Predictions not accurate for unseen_mushrooms...not exporting model to current model")
+
+if not path.exists(CURRENT_MODEL_DIR):
+    makedirs(CURRENT_MODEL_DIR)
+
+rmtree(CURRENT_MODEL_DIR)
+copytree(path.dirname(model_file_path), CURRENT_MODEL_DIR)
+
+
+
+
