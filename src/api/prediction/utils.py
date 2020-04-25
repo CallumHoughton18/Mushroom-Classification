a385f582f97968266fac7d37cbdd8ca4922fe47b
CallumHoughton18 <Callum.Houghton13@hotmail.co.uk>
2020-04-25 15:42:54 +0100

    Added json value checking for user input against range of accepted values in features.definition.json

--- src/api/prediction/utils.py
+++ src/api/prediction/utils.py
@@ -1,12 +1,18 @@
 """Utility methods for prediction functionality"""
 
-def verify_user_prediction(user_inputs_dic, correct_definition_dic):
+def verify_user_prediction(user_inputs_dic: dict, correct_definition_dic: dict):
     """
     Verifies user prediction json against correct json definition
     returns true if correct format, false if not
     """
     print(set(correct_definition_dic.keys()).difference(user_inputs_dic.keys()))
 
-    if user_inputs_dic.keys() == correct_definition_dic.keys():
-        return True
-    return False
+    if user_inputs_dic.keys() != correct_definition_dic.keys():
+        return False
+
+    for user_key, user_value in user_inputs_dic.items():
+        possible_values = correct_definition_dic[user_key]
+        if user_value not in possible_values:
+            return False
+
+    return True
